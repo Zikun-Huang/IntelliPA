6095fd5769721f3032d2c0d13080499997c86109
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2020-09-21 15:23:54 +0800

    Add project files.

---
+++ IntelliPA/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using IntelliPA.ViewModels;
+using IntelliPA.Views;
+using Xamarin.Forms;
+
+namespace IntelliPA
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(AccountEditPage), typeof(AccountEditPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ IntelliPA/Views/ItemsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using IntelliPA.Models;
+using IntelliPA.Views;
+using IntelliPA.ViewModels;
+
+namespace IntelliPA.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        //ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            //BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //_viewModel.OnAppearing();
+        }
+
+        async void OnEditButtonClicked(object sender, EventArgs args)
+        {
+            await Shell.Current.GoToAsync($"{nameof(AccountEditPage)}");
+        }
+
+        async void OnLogoutButtonClicked(object sender, EventArgs args)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
