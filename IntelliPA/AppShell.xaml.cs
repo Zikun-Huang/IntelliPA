139cc9d2ee21345a9e85d4e2b7223f55db1fcdbf
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2020-09-22 14:03:39 +0800

    Modify icon sets and add help page.

--- IntelliPA/AppShell.xaml.cs
+++ IntelliPA/AppShell.xaml.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 using IntelliPA.ViewModels;
 using IntelliPA.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace IntelliPA
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
+
         public AppShell()
         {
             InitializeComponent();
             Routing.RegisterRoute(nameof(AccountEditPage), typeof(AccountEditPage));
         }
 
-        private async void OnMenuItemClicked(object sender, EventArgs e)
+        private async void OnLogoutButtonClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        private async void OnHelpButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//HelpPage");
         }
     }
 }
